25b0d0717aa837343b21b1b31f34bd44a27e590a
za1377 <zahraanvari216@gmail.com>
2023-10-22 19:56:34 +0330

    first commit

---
+++ src/main/java/com/webservice/restapiwebservice/RestApiWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.webservice.restapiwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/webservice/restapiwebservice/helloWorld/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.webservice.restapiwebservice.helloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/{name}")
+    public HelloWorldBean helloWorldName(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello %s", name));
+    }
+}
